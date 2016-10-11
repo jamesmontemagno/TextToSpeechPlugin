97e84e0a020120aa3d5fa745d4c18b416301d114
James Montemagno <james.montemagno@gmail.com>
2016-10-11 10:17:21 +0800

    fix build issue for windows phone

--- src/TextToSpeech.Plugin.WinPhone/Properties/AssemblyInfo.cs
+++ src/TextToSpeech.Plugin.WinPhone/Properties/AssemblyInfo.cs
@@ -1,11 +1,8 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Resources;
 
 
 [assembly: AssemblyTitle("Plugin.TextToSpeech.WinPhone")]
 [assembly: AssemblyDescription("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("6ec1bd3c-a012-4a77-9f1d-bd1347953524")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
