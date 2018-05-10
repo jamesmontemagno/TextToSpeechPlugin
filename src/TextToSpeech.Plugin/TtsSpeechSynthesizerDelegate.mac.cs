4a8d56d90882a4618ce13cbe24bae8901b5971fd
James Montemagno <james.montemagno@gmail.com>
2018-05-10 10:56:20 -0700

    Update to multi-target

---
+++ src/TextToSpeech.Plugin/TtsSpeechSynthesizerDelegate.mac.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using AppKit;
+
+
+namespace Plugin.TextToSpeech
+{
+	/// <summary>
+	/// TTS delegate to listen to speach ending
+	/// </summary>
+    public class TtsSpeechSynthesizerDelegate : NSSpeechSynthesizerDelegate
+    {
+		/// <summary>
+		/// Event to be triggered when speaking finishes.
+		/// </summary>
+        public event EventHandler FinishedSpeaking;
+
+
+		/// <summary>
+		/// Invoke method when finished speaking.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="finishedSpeaking"></param>
+        public override void DidFinishSpeaking(NSSpeechSynthesizer sender, bool finishedSpeaking)
+        {
+            if (finishedSpeaking)
+                FinishedSpeaking?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
